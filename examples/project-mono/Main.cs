1963b867d28f4fef341f04670f1810dec6f85f62
Mario Steele <mario@ruby-im.net>
2024-04-07 19:31:03 -0500

    Update Main.cs
    Added Godot.Sharp.Extras code to ensure everything is working.

--- examples/project-mono/Main.cs
+++ examples/project-mono/Main.cs
@@ -1,9 +1,16 @@
 using Godot;
+using Godot.Sharp.Extras;
 
 public class Main : Control
 {
+    [NodePath] private Control _control;
+    [NodePath] private Label _label;
+    
     public override void _Ready()
     {
+        this.OnReady();
+        GD.Print($"Label: {_label}");
+        GD.Print($"Text: {_label.Text}");
         GD.Print("hello");
     }
 }
